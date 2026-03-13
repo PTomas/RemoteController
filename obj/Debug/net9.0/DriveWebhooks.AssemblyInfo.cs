42b55c52b9dc38c74a8fc09a36fd2bc493ff5414
lonelyWizard25 <p.tomas14@gmail.com>
2026-03-13 02:00:30 -0700

    Configure Azure Developer Pipeline

--- obj/Debug/net9.0/DriveWebhooks.AssemblyInfo.cs
+++ obj/Debug/net9.0/DriveWebhooks.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DriveWebhooks")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c49f0e966d441f94c3c7dfd8d294f4c039a79bee")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+05ac7e1ccac76f9b25efd97c782ab0bfac8ace13")]
 [assembly: System.Reflection.AssemblyProductAttribute("DriveWebhooks")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DriveWebhooks")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
